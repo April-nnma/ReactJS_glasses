fb261e1baea4e7af23e2fbf6d3a0cc2e2f0c19e8
Nguyen Anh <nguyenanh1904.vn@gmail.com>
2023-08-03 23:05:59 +0700

    state

---
+++ README.md
@@ -0,0 +1,5 @@
+B1: Taọ giao diện cho ứng dụng thử kính
+B2: Load những phần tử dữ liệu của giao diện từ file json
+B3: Xác định thành phần thay đổi trên giao diện => Lưu trữ vào state
+(kính ko thay đổi, model thay đổi => nguồn dữ liệu của cặp kính, thông tin của kính phải đc lưu trữ vào state)
+Nếu ko lưu trữ trong state => ko quản lý đc giá trị mỗi lần thao tác => nó sẽ render lại
---
+++ src/BaiTapThuKinh/BaiTapThuKinh.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BaiTapThuKinh = () => {
+  return (
+    // <div className="container mt-5">
+    //   <h1>BaiTapThuKinh</h1>
+    // </div>
+    <div
+      style={{
+        backgroundImage: `url("./glassesImage/background.jpg")`,
+        backgroundSize: "1600px",
+        minHeight: "2000px",
+      }}
+    ></div>
+  );
+};
+
+export default BaiTapThuKinh;
